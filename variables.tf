c64fd124b3ba69c3abbeecac9c648f6a596c964e
mamraj yadav <80300065+mamrajyadav@users.noreply.github.com>
2023-10-13 23:02:58 +0530

    fix: update ec2 module and sub module tag (#27)
    * fix: update ec2 module and sub module tag
    
    * fix: run terraform fmt
    
    * fix: update readme

--- variables.tf
+++ variables.tf
@@ -24,17 +24,16 @@ variable "environment" {
   description = "Environment (e.g. `prod`, `dev`, `staging`)."
 }
 
-variable "label_order" {
-  type        = list(any)
-  default     = []
-  description = "Label order, e.g. `name`,`application`."
-}
-
-
-variable "key_name" {
+variable "public_key" {
   type        = string
   default     = ""
   description = "The key name to use for the instance."
+}
+
+variable "user_data" {
+  type        = string
+  default     = ""
+  description = "(Optional) A string of the desired User Data for the ec2."
 }
 
 variable "ami" {
@@ -45,39 +44,7 @@ variable "ami" {
 
 variable "instance_type" {
   type        = string
-  default     = ""
   description = "The type of instance to start. Updates to this field will trigger a stop/start of the EC2 instance."
-}
-variable "ebs_optimized" {
-  type        = bool
-  default     = false
-  description = "If true, the launched EC2 instance will be EBS-optimized."
-}
-
-
-variable "monitoring" {
-  type        = bool
-  default     = true
-  description = "If true, the launched EC2 instance will have detailed monitoring enabled. (Available since v0.6.0)."
-}
-
-variable "vpc_security_group_ids_list" {
-  type        = list(string)
-  default     = []
-  description = "A list of security group IDs to associate with."
-  sensitive   = true
-}
-
-variable "tenancy" {
-  type        = string
-  default     = "default"
-  description = "The tenancy of the instance (if the instance is running in a VPC). An instance with a tenancy of dedicated runs on single-tenant hardware. The host tenancy is not supported for the import-instance command."
-}
-
-variable "instance_profile_enabled" {
-  type        = bool
-  default     = false
-  description = "Flag to control the instance profile creation."
 }
 
 variable "root_block_device" {
@@ -86,31 +53,6 @@ variable "root_block_device" {
   description = "Customize details about the root block device of the instance. See Block Devices below for details."
 }
 
-variable "associate_public_ip_address" {
-  type        = bool
-  default     = true
-  description = "Associate a public IP address with the instance."
-  sensitive   = true
-}
-
-variable "assign_eip_address" {
-  type        = bool
-  default     = false
-  description = "Assign an Elastic IP address to the instance."
-  sensitive   = true
-}
-
-variable "user_data" {
-  type        = string
-  default     = ""
-  description = "(Optional) A string of the desired User Data for the ec2."
-}
-
-variable "instance_tags" {
-  type        = map(any)
-  default     = {}
-  description = "Instance tags."
-}
 variable "ebs_volume_size" {
   type        = number
   default     = 30
@@ -121,12 +63,6 @@ variable "ebs_volume_type" {
   type        = string
   default     = "gp2"
   description = "The type of EBS volume. Can be standard, gp2 or io1."
-}
-
-variable "pritunl_enabled" {
-  type        = bool
-  default     = true
-  description = "Flag to control the instance creation."
 }
 
 variable "ebs_volume_enabled" {
@@ -142,8 +78,39 @@ variable "subnet_ids" {
   sensitive   = true
 }
 
+variable "instance_count" {
+  type        = number
+  default     = 1
+  description = "Number of instances to launch."
+}
+
 variable "iam_instance_profile" {
   type        = string
   default     = ""
   description = "The IAM Instance Profile to launch the instance with. Specified as the name of the Instance Profile."
+}
+
+variable "instance_tags" {
+  type        = map(any)
+  default     = {}
+  description = "Instance tags."
+}
+
+variable "vpc_id" {
+  type        = string
+  default     = ""
+  description = "The ID of the VPC that the instance security group belongs to."
+  sensitive   = true
+}
+
+variable "ssh_allowed_ip" {
+  type        = list(any)
+  default     = []
+  description = "List of allowed ip."
+}
+
+variable "ssh_allowed_ports" {
+  type        = list(any)
+  default     = []
+  description = "List of allowed ingress ports"
 }
